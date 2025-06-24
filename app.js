835e3efa02446f89bbc180a02ef86e0d0b2d3222
MakharashviliBeqa <201153206+MakharashviliBeqa@users.noreply.github.com>
2025-06-24 17:28:27 +0400

    Made week day Code

--- app.js
+++ app.js
@@ -29,3 +29,34 @@ for (let i = 0; i < numbers.length; i++) {
         console.log(numbers[i]);
     }
 }
+
+//
+
+const currentDay = new Date().getDay();
+
+switch (currentDay) {
+    case 0 :
+        console.log("კვირა");
+        break;
+    case 1 :
+        console.log("ორშაბათი");
+        break;
+    case 2 :
+        console.log("სამშაბათი");
+        break;
+    case 3 :
+        console.log("ოთხშაბათი");
+        break;
+    case 4 :
+        console.log("ხუთშაბათი");
+        break;
+    case 5 :
+        console.log("პარასკევი");
+        break;
+    case 6 :
+        console.log("შაბათი");
+        break;
+}
+
+//
+
